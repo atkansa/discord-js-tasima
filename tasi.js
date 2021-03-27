04e947850f313fefe7ada750070b9fc2ae633bc4
Samet <atkans26@gmail.com>
2021-03-27 20:30:55 +0300

    Add files via upload

---
+++ tasi.js
@@ -0,0 +1,34 @@
+const Discord = require('discord.js')
+const ayarlar = require('../ayarlar.json')
+
+exports.run = async (client ,message ,args) => {
+   
+ 
+   
+    if (!message.member.hasPermission('MOVE_MEMBERS')) return message.channel.send('`Bu Kodu Kullanmaya iznin Yok`');
+    const üye = message.mentions.members.first() || message.guild.members.cache.get(args[0])
+    if (!üye) return message.channel.send('`Geçerli bir üye belirtmediniz.`')
+    if (!üye.voice.channel) return message.channel.send('`Bahsettiğiniz kişi sesli kanalda değil.`');
+      if(!args[1]) {
+        if(message.member.voice.channel) return message.channel.send('`Sen seste değilsin!`');
+        message.channel.send('`Başarıyla kanala taşındı`');
+        üye.voice.setChannel(message.member.voice.channelID) 
+      } else {
+        if(!üye.voice.channel) return message.channel.send('`Belirtilen üye seste değil`');
+        message.channel.send('`Başarıyla kanala taşındı`');
+        üye.voice.setChannel(args[1])
+      }
+        
+        //developed By Atkansa
+    }
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: [],
+  permLevel: 0
+};
+//Dcs
+exports.help = {
+  name: "taşı",
+  kategori:'sahip'
+};
